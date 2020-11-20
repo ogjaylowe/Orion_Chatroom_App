2127275bf15d89ae7946fdaac2d334a98152b20e
Jay Lowe <48257497+ogjaylowe@users.noreply.github.com>
2020-11-20 12:08:03 -0800

    state lecture 1

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ export default function App() {
 
   return (
     <div className="App">
-      <input class="uk-input" type="text" placeholder="Input" />
-      <button class="uk-button uk-button-primary">Send Message</button>
+      <h1>{message}</h1>
+      <input
+        className="uk-input"
+        type="text"
+        placeholder="Input"
+        onChange={() => updateMessage("this has changed")}
+      />
+      <button
+        className="uk-button uk-button-primary"
+        onClick={() => console.log(message + " has been sent!")}
+      >
+        Send Message
+      </button>
+      <button onClick={() => updateMessage("example 2 button")}>
+        example 2
+      </button>
+      <button onClick={() => updateMessage("example 3 button")}>
+        example 3
+      </button>
+      <button onClick={() => updateMessage("example 4 button")}>
+        example 4
+      </button>
     </div>
   );
 }
